a6f375ccd7f1244eb48a59db0bee83debb864b8f
hoffor <37904227+hoffor@users.noreply.github.com>
2025-02-03 10:59:48 -0500

    Add files via upload

--- AutoMessage/Plugin.cs
+++ AutoMessage/Plugin.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using Unity.Netcode;
 using BepInEx.Configuration;
 using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace AutoMessage;
@@ -17,7 +18,7 @@ public class Plugin : BaseUnityPlugin
 {
 	public static Plugin Instance;
 	
-	internal static new ManualLogSource Logger;
+	private static readonly BepInEx.Logging.ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("AutoMessage");
 	
 	// config
 	public bool randomize;
@@ -28,13 +29,8 @@ public class Plugin : BaseUnityPlugin
 	
 	private static int messageIndex = 0;
 	
-	// FOR TESTING:
-	//public static string lastMoon = "";
-	//public static bool isFirstRun = true;
-	
 	private void Awake()
 	{
-		Logger = base.Logger;
 		if (Instance == null)
 		{
 			Instance = this;
@@ -44,13 +40,11 @@ public class Plugin : BaseUnityPlugin
 		
 		new Harmony(MyPluginInfo.PLUGIN_GUID).PatchAll(Assembly.GetExecutingAssembly());
 
-		Logger.LogInfo($"{MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION} loaded.");
+		Log.LogInfo($"{MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION} loaded.");
 	}
 	
 	public void MakeConfig()
 	{
-		Logger = base.Logger;
-		
 		ConfigEntry<string> randomizeConfig = Config.Bind("1. Randomize", // Category
 			"Randomize", // Name
 			"false", // Default
@@ -99,47 +93,117 @@ public class Plugin : BaseUnityPlugin
 }
 
 
-
 [HarmonyPatch(typeof(StartOfRound))]
 public class StartOfRoundPatch
 {
+	private static readonly BepInEx.Logging.ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource("AutoMessage");
+	
+	private static RectTransform foundObjectRectTransform;
+	private static GameObject foundObject;
+	
+	private static Vector3 startingPosition;
+	private static int count;
+	
 	[HarmonyPrefix] // prefix so it runs before the original method, otherwise will show after moon is finished loading
 	[HarmonyPatch("SceneManager_OnLoad")]
 	public static void OnStartGame()
 	{
+		// adjust tips panel size and positioning so it's more visible behind the overlays that appear and so more text can fit
+		// this is inefficient ugly code but also i am very tired
+		foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image");
+		if (foundObject != null)
+		{
+			foundObjectRectTransform = foundObject.GetComponent<RectTransform>();
+			if (foundObjectRectTransform != null)
+			{
+				foundObjectRectTransform.anchorMin = new Vector2(0.6f, 1.1f); // high enough to not be covered by a scanner text box
+				foundObjectRectTransform.anchorMax = new Vector2(0.6f, 1.1f);
+				foundObjectRectTransform.sizeDelta = new Vector2(345.8f, 200f);
+			}
+		}
+		foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image/Header");
+		if (foundObject != null)
+		{
+			foundObjectRectTransform = foundObject.GetComponent<RectTransform>();
+			if (foundObjectRectTransform != null)
+			{
+				foundObjectRectTransform.anchorMin = new Vector2(0.5f, 0.72f);
+				foundObjectRectTransform.anchorMax = new Vector2(0.5f, 0.72f);
+			}
+		}
+		foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image/BodyText");
+		if (foundObject != null)
+		{
+			foundObjectRectTransform = foundObject.GetComponent<RectTransform>();
+			if (foundObjectRectTransform != null)
+			{
+				foundObjectRectTransform.anchorMin = new Vector2(0.5f, 0.7f);
+				foundObjectRectTransform.anchorMax = new Vector2(0.5f, 0.7f);
+			}
+		}
+		
 		string message = Plugin.Instance.GetNextMessage();
 		string title = Plugin.Instance.title;
 		Plugin.Instance.DebugLog(title, message);
-
-	}
-}
-
-/*
-// FOR TESTING:
-[HarmonyPatch(typeof(StartOfRound))]
-public class MoonSelectionPatch
-{
-	[HarmonyPostfix]
-	[HarmonyPatch("ChangeLevel")]
-	public static void OnMoonSelection(StartOfRound __instance)
-	{
-		string currentMoon = __instance.currentLevel.PlanetName;
 		
-		if (Plugin.lastMoon != currentMoon)
+		GameObject tempGameObject = new GameObject("TempMonoBehaviour");
+		MonoBehaviour tempMonoBehaviour = tempGameObject.AddComponent<TempCoroutineRunner>();
+		tempMonoBehaviour.StartCoroutine(CheckLocalPositionCoroutine()); // wait for the panel to disappear, then reset its values
+	}
+	
+	private static IEnumerator CheckLocalPositionCoroutine()
+	{
+		yield return new WaitForSeconds(3f); // wait long enough so that the panel animation stops twitching so we can get a concrete position
+		
+		foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image");
+		if (foundObject != null)
 		{
-			if (!Plugin.isFirstRun) // currentMoon will update when starting a session, don't want a notif for that
+			startingPosition = foundObject.transform.localPosition;
+			
+			count = 0;
+			while (foundObject.transform.localPosition == startingPosition) // once the panel's position has changed that means it's done with its animation aka invisible
 			{
-				string message = Plugin.Instance.GetNextMessage();
-				string title = Plugin.Instance.title;
-				Plugin.Instance.DebugLog(title, message);
+				yield return new WaitForSeconds(0.2f);
+				count++;
+				if (count >= 50)
+				{
+					break; // timeout
+				}
 			}
-			else
+			
+			foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image");
+			if (foundObject != null)
 			{
-				Plugin.isFirstRun = false;
+				foundObjectRectTransform = foundObject.GetComponent<RectTransform>();
+				if (foundObjectRectTransform != null)
+				{
+					foundObjectRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+					foundObjectRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+					foundObjectRectTransform.sizeDelta = new Vector2(345.8f, 100f);
+				}
 			}
-
-			Plugin.lastMoon = currentMoon;
+			foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image/Header");
+			if (foundObject != null)
+			{
+				foundObjectRectTransform = foundObject.GetComponent<RectTransform>();
+				if (foundObjectRectTransform != null)
+				{
+					foundObjectRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+					foundObjectRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+				}
+			}
+			foundObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/SpecialHUDGraphics/HintPanelContainer/Image/BodyText");
+			if (foundObject != null)
+			{
+				foundObjectRectTransform = foundObject.GetComponent<RectTransform>();
+				if (foundObjectRectTransform != null)
+				{
+					foundObjectRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+					foundObjectRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+				}
+			}
 		}
 	}
+	
+	private class TempCoroutineRunner : MonoBehaviour { } // temporary class to run coroutines also i barely know what i'm doing also i hope you're having a great day
 }
-*/
